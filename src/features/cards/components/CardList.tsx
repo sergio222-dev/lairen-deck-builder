982a0b386e1ec6c2e2b6d9b3baf98a42ee091b7c
sergio222-dev <molinasergio91@gmail.com>
2024-08-13 01:54:01 -0300

    fixed containers

--- src/features/cards/components/CardList.tsx
+++ src/features/cards/components/CardList.tsx
@@ -8,7 +8,7 @@ export const CardList = component$<CardListProps>(() => {
   const c = useContext(FilterContext);
 
   return (
-    <div class="sm:p-4 shadow-lg sm:m-4 flex flex-wrap justify-around">
+    <div class="sm:p-0 shadow-lg flex flex-wrap justify-around">
       {c.cards.map((card) => (
         <div class="p-4 flex w-1/2 md:w-1/4 lg:w-1/5" key={card.id}>
           <img loading='lazy' width={400} height={400} alt={card.name} src={card.image} />
