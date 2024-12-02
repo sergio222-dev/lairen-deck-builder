468ce747dcb0fb23d5228b0caa6f7b7661a249bd
Agustín Barcia <agustinbarcia.dev@gmail.com>
2024-12-02 12:52:23 -0300

    - fixed preview close button z-index

--- src/features/cardPreview/CardPreview.tsx
+++ src/features/cardPreview/CardPreview.tsx
@@ -28,10 +28,10 @@ export const CardPreview = component$(() => {
         >
             {/* Close Button */}
             <ButtonIcon
-                class="absolute right-8 top-8 cursor-pointer rounded-full p-2 transition duration-300 ease-in-out hover:bg-secondary"
+                class="absolute right-8 top-8 cursor-pointer z-10 rounded-full p-2 transition duration-300 ease-in-out hover:bg-secondary"
                 onClick$={handleCloseDialog}
             >
-                <Icon name="close" width={32} height={32} class="z-10 fill-white" />
+                <Icon name="close" width={32} height={32} class="fill-white" />
             </ButtonIcon>
 
             {/* Card Detail Container */}
