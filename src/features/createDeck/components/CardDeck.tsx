0e6f9746acd4338ff5d8859da9af35272a988b9d
Agustín Barcia <agustinbarcia.dev@gmail.com>
2024-12-02 12:03:39 -0300

    - now card is added to main deck when clicked

--- src/features/createDeck/components/CardDeck.tsx
+++ src/features/createDeck/components/CardDeck.tsx
@@ -28,7 +28,7 @@ export const CardDeck = component$<CardDeckProps>(({ card }) => {
     return (
         <div class="w-[calc(50%-0.5rem)] sm:w-[calc(50%-0.5rem)] md:w-[calc(100%-0.5rem)] lg:w-[calc(50%-0.5rem)] xl:w-[calc(33%-0.5rem)] 2xl:w-[calc(33%-0.5rem)]">
             <div
-                class={`group relative aspect-[2.5/3.5] cursor-pointer rounded-[5%/3.571428571428571%] bg-[length:100%_auto] bg-[50%_25%]
+                class={`group select-none relative aspect-[2.5/3.5] cursor-pointer rounded-[5%/3.571428571428571%] bg-[length:100%_auto] bg-[50%_25%]
        bg-no-repeat ring-secondary transition-all hover:animate-wiggle hover:bg-[length:100%_auto] hover:shadow-[0px_0px_10px_3px] hover:shadow-secondary hover:ring-1`}
                 key={card.id}
                 style={{
