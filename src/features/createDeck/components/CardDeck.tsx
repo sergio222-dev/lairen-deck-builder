4aefb15c75dcef86ea9d0049226d032fa5a0a1c0
Agustín Barcia <agustinbarcia.dev@gmail.com>
2024-12-02 12:45:46 -0300

    fixed event propagation when clicking preview button

--- src/features/createDeck/components/CardDeck.tsx
+++ src/features/createDeck/components/CardDeck.tsx
@@ -39,10 +39,11 @@ export const CardDeck = component$<CardDeckProps>(({ card }) => {
                 onClick$={() => d.addCard(card, false)}
             >
                 <div
-                    class="absolute right-2 top-2 z-10 flex h-12 w-12 ring-2 ring-secondary cursor-pointer items-center justify-center rounded-full bg-white bg-opacity-90 opacity-0 shadow-lg transition-all duration-200 hover:scale-110 hover:bg-opacity-[97%] group-hover:opacity-100"
-                    onClick$={() => {
+                    class="absolute right-2 top-2 z-10 flex h-12 w-12 ring-2 ring-secondary cursor-pointer items-center justify-center rounded-full bg-white bg-opacity-75 opacity-0 shadow-lg transition-all duration-200 hover:scale-110 hover:bg-opacity-90 group-hover:opacity-100"
+                    onClick$={(event) => {
                         void cardViewer.setCard(card);
                         cardViewer.isOpen = true;
+                        event.stopPropagation();
                     }}
                 >
                     <Icon name="magnifying-glass" height={28} width={28} class={"fill-secondary"} />
