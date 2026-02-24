62a931f654a655c034b16c58b3d184e4cede7815
sergio222-dev <molinasergio91@gmail.com>
2026-02-24 19:26:30 -0300

    feat/deck_card_update - new deck card looks - added new super user

--- src/features/deckList/DeckList.tsx
+++ src/features/deckList/DeckList.tsx
@@ -6,7 +6,7 @@ export const DeckList = component$(() => {
     const dl = useContext(DECK_LIST_CONTEXT);
 
   return (
-    <div class="flex p-2 flex-wrap gap-[0.5rem]">
+    <div class="flex p-2 justify-center flex-wrap xl:gap-[2rem] md:gap-4">
       {dl.decks.map(d => {
         return (
           <DeckCard id={d.deckId} name={d.name} splashArt={d.splashArt} key={d.deckId}/>
--- src/lib/constants/qwik.ts
+++ src/lib/constants/qwik.ts
@@ -1,4 +1,4 @@
 export const QWIK_CONSTANTS = {
   REQUEST_USER: 'REQUEST_USER',
-  SUPER_USERS_MAIL: ['molinasergio91@gmail.com', 'lautaro.bebek9@gmail.com']
+  SUPER_USERS_MAIL: ['molinasergio91@gmail.com', 'lautaro.bebek9@gmail.com', 'marquinuz69@gmail.com']
 }
